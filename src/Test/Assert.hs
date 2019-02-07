724b59d46127a10d33b099f6f24156d3ee60d8d0
Arnaud Spiwack <arnaud@spiwack.net>
2019-02-07 21:58:18 +0100

    feat: assertions can be expressed in many types

--- src/Test/Assert.hs
+++ src/Test/Assert.hs
@@ -1,8 +1,24 @@
 module Test.Assert where
 
-assert :: a -> a
-assert x = x
+import GHC.Stack
+
+assert :: (HasCallStack, IsAssertion v) => v -> a -> a
+assert _ x = x
 {-# INLINE assert #-}
 
-assertError :: a -> a
-assertError _ = error "It works"
+assertError :: (HasCallStack, IsAssertion v) => v -> a -> a
+assertError v x =
+  case checkAssertion v of
+    Passed -> x
+    Failed msg -> error ("Assertion failure: " ++ msg)
+
+data Result
+  = Passed
+  | Failed String
+
+class IsAssertion v where
+  checkAssertion :: v -> Result
+
+instance IsAssertion Bool where
+  checkAssertion True = Passed
+  checkAssertion False = Failed "boolean test"
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,5 +7,7 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "Replacing assert with assertError" $ do
-    it "is performed" $ do
-      evaluate (assert ()) `shouldThrow` anyException
+    it "accepts passed assertions" $ do
+      assert True 17 `shouldBe` 17
+    it "throws an error on a failed assertion" $ do
+      evaluate (assert False ()) `shouldThrow` anyException
